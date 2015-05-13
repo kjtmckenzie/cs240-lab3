76f1125e32b2d41278eedc5d208b9e582fcd34bd
limsk1 <limsk1@stanford.edu>
2015-05-13 03:26:49 +0000

    Now tracer_sample accepts four arguments we want

--- test/tracer_sample.c
+++ test/tracer_sample.c
@@ -8,15 +8,40 @@
 #include <sys/syscall.h>
 #include <sys/reg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define TARGET "/home/vagrant/lab3/test/target_sample"
-#define NEW_UID 0
+#define MAX_TARGET_LEN 255
 
+//#define TARGET "/home/vagrant/lab3/test/target_sample"
+//#define NEW_UID 0
 // syscall SYS_getuid == 102
-#define TARGET_SYSCALL 102
+//#define TARGET_SYSCALL 102
 
+int target_syscall;
+long long int retval;
+double prob;
+char target[MAX_TARGET_LEN + 1];
 
-int main() {
+int main(int argc, char *argv[]) {
+  if(argc != 5) {
+    printf("Insufficient number of argument.\n");
+    //print_usage();
+    return 1;
+  }
+
+  if(strlen(argv[4]) > MAX_TARGET_LEN) {
+    printf("Target name cannot be longer than 255.\n");
+    return 1;
+  }
+
+  target_syscall = atoi(argv[1]);
+  retval = atoll(argv[2]);
+  prob = atof(argv[3]);
+  strncpy(target, argv[4], MAX_TARGET_LEN);
+
+  srand(time(NULL));
+
   int status = 0;
   int syscall_n = 0;
   int entering = 1;
@@ -25,7 +50,7 @@ int main() {
 
   if ( !pid ) {
     ptrace( PTRACE_TRACEME, 0, 0, 0 );
-    execlp( TARGET, TARGET, NULL );
+    execlp( target, target, NULL );
   }
   else {
     wait( &status );
@@ -42,18 +67,23 @@ int main() {
       // get syscall number
       syscall_n = regs.orig_rax;
       //printf("Syscall is %d\n", syscall_n);
-      
+     
       // only intercept the syscall we want to intercept
-      if ( syscall_n == TARGET_SYSCALL ) {
+      if ( syscall_n == target_syscall ) {
         if ( entering ) {
           // we only want to change the return value on syscall exit
           entering = 0;
         }
         else {
           ptrace( PTRACE_GETREGS, pid, 0, &regs );
-          // replace the return value with our own 
-          regs.rax = 0;
-        
+          //printf("Target syscall %d caught.\n", target_syscall);
+          //printf("Current return value is 0x%016llx.\n", regs.rax);
+          //printf("Type the return value you want to change in hex.\n");
+          //scanf("%llx", &regs.rax);
+
+          if(((double)rand() / RAND_MAX) < prob)
+            regs.rax = retval;
+
           // set the return value of the syscall
           ptrace( PTRACE_SETREGS, pid, 0, &regs );
           entering = 1;
