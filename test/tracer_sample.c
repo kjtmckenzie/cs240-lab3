b2802ff1a096ad61501ee403f57efa5c2a024e9e
Patrick Harvey <p1harvey@stanford.edu>
2015-05-15 16:52:25 -0700

    Added -std=c99 flag to Makefile for for loop declarations; some refactoring and cleanup on tracer_sample as well as adding multi-run mode

--- test/tracer_sample.c
+++ test/tracer_sample.c
@@ -13,34 +13,32 @@
 
 #define MAX_TARGET_LEN 255
 
-#define NUM_ARGS 5
+#define MAX_ARGS 6
+#define MIN_ARGS 4
+
+#define FULL_RUN_MODE 0
+#define RUN_MODE 1
+#define SKIP_MODE 2
+
+//A return value indicating we reached past the end of executable. Chosen larger than 256 so that it 
+//won't clash with most POSIX exit codes.
+#define END_OF_EXECUTABLE 400
 
 void print_usage_and_exit() {
-  printf("Usage: tracer_sample signum retval prob target\n");
+  printf("Usage: tracer_sample signum retval [ -s num_to_skip || -r num_of_runs ] target\n");
   printf("    signum: Signal # to intercept\n");
   printf("    retval: Return value to insert\n");
   printf("    num_to_skip: Number of syscalls to skip before injection\n");
+  printf("    num_of_runs: Runs in multi-run mode, skipping first 0 syscalls before fault injection, then 1, 2, etc.. up to num_of_runs\n");
   printf("    target: Path to target executable\n");
+  printf("Default behavior for skip/runs is to run until no faults are injected.\n");
   exit(1);
 }
 
-int main(int argc, char *argv[]) {
-  if(argc != NUM_ARGS) {
-    printf("Wrong number of arguments: %d for %d.\n", argc, NUM_ARGS);
-    print_usage_and_exit();
-  }
-
-  if(strlen(argv[4]) > MAX_TARGET_LEN) {
-    printf("Target name cannot be longer than 255.\n");
-    return 1;
-  }
-
-  int target_syscall = atoi(argv[1]);
-  long long int retval = atoll(argv[2]);
-  long long int num_to_skip = atoll(argv[3]);
-  char target[MAX_TARGET_LEN + 1];
-  strncpy(target, argv[4], MAX_TARGET_LEN);
-  
+/* Perform a single run of tracing, skipping the first num_to_skip syscalls and injecting a fault in all those 
+   that follow. */
+int single_injection_run(int target_syscall, long long int retval, long long int num_to_skip,
+			 char* target) {
   int status = 0;
   int syscall_n = 0;
   int entering = 1;
@@ -54,7 +52,7 @@ int main(int argc, char *argv[]) {
   }
   else {
     wait( &status );
-    
+
     while ( 1 ) {
       ptrace( PTRACE_SYSCALL, pid, 0, 0 );
       wait( &status );
@@ -62,10 +60,10 @@ int main(int argc, char *argv[]) {
       if ( WIFEXITED( status ) ) break;
 
       ptrace( PTRACE_GETREGS, pid, 0, &regs );
-      
+
       // get syscall number
       syscall_n = regs.orig_rax;
-       
+
       // only intercept the syscall we want to intercept
       if ( syscall_n == target_syscall ) {
         if ( entering ) {
@@ -76,12 +74,7 @@ int main(int argc, char *argv[]) {
           syscall_count++;
           if (syscall_count > num_to_skip) {
             ptrace( PTRACE_GETREGS, pid, 0, &regs );
-            //printf("Target syscall %d caught.\n", target_syscall);
-            //printf("Current return value is 0x%016llx.\n", regs.rax);
-            //printf("Type the return value you want to change in hex.\n");
-            //scanf("%llx", &regs.rax);
-            regs.rax = retval;
-
+	    regs.rax = retval;
             // set the return value of the syscall
             ptrace( PTRACE_SETREGS, pid, 0, &regs );
             entering = 1;
@@ -90,6 +83,94 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+  if (syscall_count <= num_to_skip) { // If num_to_skip was so high no faults were injected.
+    return END_OF_EXECUTABLE; 
+  }
 
   return 0;
+}
+
+/* Run injections progressing from faulting the first syscall, to the second, third, etc... until 
+   the runs have faulted every syscall in the execution once. */
+int full_injection_run(int target_syscall, long long int retval, char* target) {
+  long long int current_skip = 0;
+  
+  int res = 0;
+  while (res == 0) {
+    res = single_injection_run(target_syscall, retval, current_skip, target);
+    current_skip++;
+  }
+  return res;
+}
+
+/* Run injections progressing from faulting the first syscall to the second, third, etc... until
+   either all syscall in the execution have been faulted or all syscalls up to the input num_ops have been 
+   faulted, whichever comes first. */
+int multi_injection_run(int target_syscall, long long int retval, long long int num_ops, char* target) {
+  for (long long int i = 0; i <= num_ops; i++) {
+    int res = single_injection_run(target_syscall, retval, i, target);
+    if (res) { //End if an error w.r.t the injector's end occurs or we reach past the end of the executable.
+      return res;
+    }
+  }
+  return 0;
+}
+
+/* Launch the program. */
+int main(int argc, char *argv[]) {
+  // Validate argc
+  if((argc != MIN_ARGS) && (argc != MAX_ARGS)) {
+    print_usage_and_exit();
+  }
+
+  // Validate target name
+  char* target_inp;
+  if (argc == MIN_ARGS) {
+    target_inp = argv[3];
+  } else {
+    target_inp = argv[5];
+  }
+  if(strlen(target_inp) > MAX_TARGET_LEN) {
+    printf("Target name cannot be longer than 255.\n");
+    return 1;
+  }
+
+  int target_syscall = atoi(argv[1]);
+  long long int retval = atoll(argv[2]);
+
+  int mode = FULL_RUN_MODE;
+  long long int num_ops;
+
+  // Determine the mode of operation and the number of skips/runs if applicable.
+  if (argc == MAX_ARGS) {
+    num_ops = atoll(argv[4]);
+    if (!strcmp(argv[3], "-s")) {
+      mode = SKIP_MODE;
+    } else if (!strcmp(argv[3], "-r")) {
+      mode = RUN_MODE;
+    } else {
+      print_usage_and_exit();
+    }
+  }
+
+  // Create a buffer to store the target name.
+  char target[strlen(target_inp) + 1];
+  strcpy(target, target_inp);
+
+  int rval = 0;
+
+  // Dispatch the runs.
+  if (mode == FULL_RUN_MODE) {
+    rval = full_injection_run(target_syscall, retval, target);
+  } else if (mode == RUN_MODE) {
+    rval = multi_injection_run(target_syscall, retval, num_ops, target);
+  } else {
+    rval = single_injection_run(target_syscall, retval, num_ops, target);
+  }
+
+  // END_OF_EXECUTABLE is an internal signal, not an external one.
+  if (rval == END_OF_EXECUTABLE) {
+    return 0;
+  }
+  return rval;
 }
