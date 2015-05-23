95681199f53bb24f758fb48e462faada14403d7e
dokwon <dokwon@stanford.edu>
2015-05-22 17:59:12 -0700

    did address parsing of fn callsites

--- test/malloc_tracer.c
+++ test/malloc_tracer.c
@@ -20,17 +20,69 @@
 #define BUFLEN 4096
 #define MAX_TARGET_LEN 255
 
-#define NUM_ARGS 4
+#define NUM_ARGS 5
 
 void print_usage_and_exit() {
-  printf("Usage: injector fn retval target\n");
+  printf("Usage: injector fn retval target callnum\n");
   printf("Where:\n");
-  printf("        fn = function to inject, e.g. 'malloc'\n");
-  printf("    retval = return value to inject, e.g. '0'\n");
-  printf("    target = statically-compiled target executable, e.g. 'bin/malloc_target'\n");
+  printf("        fn  = function to inject, e.g. 'malloc'\n");
+  printf("    retval  = return value to inject, e.g. '0'\n");
+  printf("    target  = statically-compiled target executable, e.g. 'bin/malloc_target'\n");
+  printf("    callnum = callnum_th call will be intercepted\n");
   exit(1);
 }
 
+unsigned long long*get_target_addrs(const char *fn, const char *target) {
+  char buf[BUFLEN];
+  memset(buf, 0, BUFLEN);
+  snprintf(buf, BUFLEN - 4, "objdump -D %s | grep '%s'", target, fn);
+
+  FILE *fp = popen(buf, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Execution of \"%s\" failed; couldn't read symtab of target!\n", buf);
+    //return NULL;
+    return NULL;
+  } else if (strlen(fn) > BUFLEN - 3) {
+    fprintf(stderr, "Target function name '%s' is too long!\n", fn);
+    pclose(fp);
+    //return NULL;
+    return NULL;
+  }
+
+
+  unsigned long long *addrs = malloc (sizeof (unsigned long long) * 1000);
+  size_t addr_idx = 0; 
+
+  // Read each line looking for the address of fn
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  memset(buf, 0, BUFLEN);
+
+  void *addr = NULL;
+  while ((read = getline(&line, &len, fp)) != -1) {
+    if (strstr(line, ":")) {
+      // This is the line for our fn: now try to read the address
+      if (strstr(line, " U ")) {
+        fprintf(stderr, "No address in the symbol table for '%s'! Make sure the target was compiled with '-static'!\n", fn);
+      } else {
+
+        unsigned long long val;
+        if (sscanf(line, "%16llx", &val) <= 0) {
+          fprintf(stderr, "Couldn't parse address from line '%s'\n", line);
+        } else {
+          // Got the address
+          addrs[addr_idx] = val;
+          addr_idx ++;
+        }
+      }
+    }
+  }
+  // Sentinel value for end of addresses
+  addrs[addr_idx] = (unsigned long long) 0;
+  return addrs;
+}
 /**
  * TODO: Okay, so now this function can retrieve the address of first instuction
  *       of target function. What we need to retrieve is the address of last
@@ -101,6 +153,8 @@ void reset_trap (
 }
 
 int main(int argc, char *argv[]) {
+  
+
   if(argc != NUM_ARGS) {
     printf("Wrong number of arguments: %d for %d.\n", argc, NUM_ARGS);
     print_usage_and_exit();
@@ -115,12 +169,30 @@ int main(int argc, char *argv[]) {
   long long int retval = atoll(argv[2]);
   char target[MAX_TARGET_LEN + 1];
   strncpy(target, argv[3], MAX_TARGET_LEN);
+  int callnum = atoi (argv[4]);
+
+  get_target_addrs(fn, target);
 
   int status = 0;
   int signum = 0;
 
+  unsigned long long *addrs = get_target_addrs (fn, target);
+  
+  // Something went wrong in retrieving addrs...
+  if (!addrs) {
+    exit(-1);
+  }
+
+  for (int i = 0 ; addrs[i] != 0; i ++ ) {
+    unsigned long long addr = addrs[i];
+    printf ("%16llx\n", addr);
+  }
+
+  // TODO: Do something with the parsed addresses
+
+
   //unsigned long long addr = get_target_address(fn, target);
-  unsigned long long addr = 0x414504;
+  /*unsigned long long addr = 0x414504;
 
   struct user_regs_struct regs;
   int pid = fork();
@@ -162,7 +234,7 @@ int main(int argc, char *argv[]) {
         ptrace(PTRACE_CONT, pid, 0, signum);
       } 
     }
-  }
+  }*/
 
   return 0;
 }
