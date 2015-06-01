f2495440cd38a39e27fa5bf974868adfc635e7d1
Kevin McKenzie <kjtmckenzie@gmail.com>
2015-06-01 00:14:12 +0000

    added functionality to follow cloned and forked process

--- test/tracer_sample.c
+++ test/tracer_sample.c
@@ -15,8 +15,8 @@
 #define MAX_TARGET_LEN 1024
 #define MAX_TARGET_ARGS 64
 
-#define MAX_ARGS 7
-#define MIN_ARGS 5
+#define MAX_ARGS 8
+#define MIN_ARGS 6
 
 #define FULL_RUN_MODE 0
 #define RUN_MODE 1
@@ -30,10 +30,11 @@
 #define END_OF_EXECUTABLE 400
 
 void print_usage_and_exit() {
-  printf("Usage: tracer_sample signum[,signum2,signum3,...] retval[,retval2,retval3,...] fail_on_entry [ -s num_to_skip || -r num_of_runs ] target\n");
+  printf("Usage: tracer_sample signum[,signum2,signum3,...] retval[,retval2,retval3,...] fail_on_entry follow_clones [ -s num_to_skip || -r num_of_runs ] target\n");
   printf("    signum: Syscall #s to intercept\n");
   printf("    retval: Return value to insert. Should be one retval per signum\n");
   printf("    fail_on_entry: 1 to fail syscall on entry or 0 to fail syscall after it has returned\n");
+  printf("    follow_clones: 1 to trace cloned or forked processes or 0 to only trace the original process\n");
   printf("    num_to_skip: Number of syscalls to skip before injection\n");
   printf("    num_of_runs: Runs in multi-run mode, skipping first 0 syscalls before fault injection, then 1, 2, etc.. up to num_of_runs\n");
   printf("    target: Path to target executable. Include any command-line args to run with\n");
@@ -46,10 +47,42 @@ int cmp_sys_num(const void* num_a, const void* num_b) {
   return (*(int*)num_a) - (*(int*)num_b);
 }
 
+int clone_entering = 1;
+/* Get PID of cloned process.  If not process was cloned or an error occured, return -1 */
+/* pid is the process id of the current traced process */
+int trace_clone(long pid) {
+  long newpid, trace;
+  int syscall_n;
+  struct user_regs_struct regs;
+  ptrace( PTRACE_GETREGS, pid, 0, &regs );
+  syscall_n = regs.orig_rax;
+  if (syscall_n == 56) {
+    if (clone_entering) {
+      clone_entering = 0;
+    } else {
+      clone_entering = 1;
+      newpid = regs.rax;
+      //ptrace(PTRACE_DETACH,pid,NULL,NULL);
+      trace = ptrace(PTRACE_ATTACH,newpid,NULL,NULL);
+      ptrace( PTRACE_SYSCALL, newpid, 0, 0 );
+      if(trace == 0) {
+        //printf("\e[1;32mAttached to offspring %ld\n\e[0m", newpid);  
+        //fflush(stdout);
+        return newpid;
+      } else {
+        printf("Could not attach to the child, trace = %ld\n", trace);
+        fflush(stdout);
+        return -1;
+      }
+    }
+  }
+  return -1;
+}
+
 /* Perform a single run of tracing, skipping the first num_to_skip syscalls and injecting a fault in all those 
    that follow. */
 int single_injection_run(int* target_syscalls, int num_syscalls, long long int* retvals,
-			 int fail_on_entry, long long int num_to_skip, char* target, char** args) {
+			 int fail_on_entry, int follow_clones, long long int num_to_skip, char* target, char** args) {
   int status = 0;
   int syscall_n = 0;
   int entering = 1;
@@ -57,6 +90,7 @@ int single_injection_run(int* target_syscalls, int num_syscalls, long long int* 
   int intercepted_retval = 0;
   long long int syscall_count = 0;
   struct user_regs_struct regs;
+  int cloned_pid;
   int pid = fork();
   if ( !pid ) {
     printf("The child is running\n");
@@ -79,12 +113,21 @@ int single_injection_run(int* target_syscalls, int num_syscalls, long long int* 
 
       if ( WIFEXITED( status ) ) break;
       else { 
-        sleep(1);
+        /* I'm not sure this code works as intended */
+        //sleep(1);
         loop_counter ++; 
-        if (loop_counter > 100) {
+        if (loop_counter > 1000000) {
           printf("TIMEOUT: Ptrace is taking too long on %s for syscall %d\n", target, syscall_n);
           exit(-1);
         }
+        
+      }
+      
+      // check to see if the process has cloned itself
+      if (follow_clones) {
+        cloned_pid = trace_clone(pid);
+        if (cloned_pid > 0)
+          pid = cloned_pid;
       }
 
       ptrace( PTRACE_GETREGS, pid, 0, &regs );
@@ -141,12 +184,12 @@ int single_injection_run(int* target_syscalls, int num_syscalls, long long int* 
 /* Run injections progressing from faulting the first syscall, to the second, third, etc... until 
    the runs have faulted every syscall in the execution once. */
 int full_injection_run(int* target_syscalls, int num_syscalls, long long int* retvals, 
-		       int fail_on_entry, char* target, char** args) {
+		       int fail_on_entry, int follow_clones, char* target, char** args) {
   long long int current_skip = 0;
   
   int res = 0;
   while (res == 0) {
-    res = single_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, current_skip, target, args);
+    res = single_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, follow_clones, current_skip, target, args);
     current_skip++;
   }
   return res;
@@ -156,9 +199,9 @@ int full_injection_run(int* target_syscalls, int num_syscalls, long long int* re
    either all syscall in the execution have been faulted or all syscalls up to the input num_ops have been 
    faulted, whichever comes first. */
 int multi_injection_run(int* target_syscalls, int num_syscalls, long long int* retvals, 
-			int fail_on_entry, long long int num_ops, char* target, char** args) {
+			int fail_on_entry, int follow_clones, long long int num_ops, char* target, char** args) {
   for (long long int i = 0; i <= num_ops; i++) {
-    int res = single_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, i, target, args);
+    int res = single_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, follow_clones, i, target, args);
     if (res) { //End if an error w.r.t the injector's end occurs or we reach past the end of the executable.
       return res;
     }
@@ -255,9 +298,9 @@ int main(int argc, char *argv[]) {
   // Validate target name
   char* target_inp;
   if (argc == MIN_ARGS) {
-    target_inp = argv[4];
+    target_inp = argv[5];
   } else {
-    target_inp = argv[6];
+    target_inp = argv[7];
   }
   if(strlen(target_inp) > MAX_TARGET_LEN) {
     printf("Target command cannot be longer than 1024.\n");
@@ -273,16 +316,17 @@ int main(int argc, char *argv[]) {
   }
 
   int fail_on_entry = atoi(argv[3]);
+  int follow_clones = atoi(argv[4]);
 
   int mode = FULL_RUN_MODE;
   long long int num_ops;
 
   // Determine the mode of operation and the number of skips/runs if applicable.
   if (argc == MAX_ARGS) {
-    num_ops = atoll(argv[5]);
-    if (!strcmp(argv[4], "-s")) {
+    num_ops = atoll(argv[6]);
+    if (!strcmp(argv[5], "-s")) {
       mode = SKIP_MODE;
-    } else if (!strcmp(argv[4], "-r")) {
+    } else if (!strcmp(argv[5], "-r")) {
       mode = RUN_MODE;
     } else {
       print_usage_and_exit();
@@ -298,11 +342,11 @@ int main(int argc, char *argv[]) {
 
   // Dispatch the runs.
   if (mode == FULL_RUN_MODE) {
-    rval = full_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, target, args);
+    rval = full_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, follow_clones, target, args);
   } else if (mode == RUN_MODE) {
-    rval = multi_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, num_ops, target, args);
+    rval = multi_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, follow_clones, num_ops, target, args);
   } else {
-    rval = single_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, num_ops, target, args);
+    rval = single_injection_run(target_syscalls, num_syscalls, retvals, fail_on_entry, follow_clones, num_ops, target, args);
   }
 
   //Free dynamic memory used for syscall numbers and injected values.
